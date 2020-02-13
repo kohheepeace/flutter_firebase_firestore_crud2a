e3c8df499ba8c67375e7fecd0f276207c7eaf1ed
kohheepeace <kohheepeace@gmail.com>
2020-02-13 14:28:59 +0700

    Finish step4-2 MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hello World!', textDirection: TextDirection.ltr,)
+    return  MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World!')
+        ),
+      ),
     );
   }
 }
