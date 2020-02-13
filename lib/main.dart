b73baeff22a328602efcbf1335f8e8568e616441
kohheepeace <kohheepeace@gmail.com>
2020-02-13 15:26:05 +0700

    Finish step4-3 statefulwidgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World!')
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World!')
       ),
     );
   }
