3d7934322bb6cdd98dd6b809911b9088540cfa49
kohheepeace <kohheepeace@gmail.com>
2020-02-17 03:06:35 +0700

    Step8-5 Fix RenderFlex Error

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -35,96 +35,98 @@ class _RegisterPageState extends State<RegisterPage> {
       appBar: AppBar(
         title: Text("Register Page"),
       ),
-      body: Form(
-        key: _registerFormKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(labelText: 'Name', hintText: "John Jackson"),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Please enter name.";
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email', hintText: "johnjackson@example.com"),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter email';
-                  } else if (!EmailValidator.validate(value)) {
-                    // Use plugin https://pub.dev/packages/email_validator
-                    // If you don't want to use plugin https://stackoverflow.com/questions/16800540/validate-email-address-in-dart
-                    return 'Please enter valid email';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter password';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _confirmPasswordController,
-                decoration: InputDecoration(labelText: 'Confirm Password'),
-                obscureText: true,
-                validator: (value) {
-                  // https://stackoverflow.com/questions/50155348/how-to-validate-a-form-field-based-on-the-value-of-the-other
-                  if (value != _passwordController.text) {
-                    return 'Password is not matching';
-                  }
-                  return null;
-                },
-              ),
-              Container(
-                // https://stackoverflow.com/questions/50186555/how-to-set-margin-for-a-button-in-flutter
-                margin: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-                child: RaisedButton(
-                  child: Text("Register"),
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  onPressed: () async {
-                    if (_registerFormKey.currentState.validate()) {
-                      try {
-                        // Register user by firebase auth
-                        final FirebaseUser user = (await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                            email: _emailController.text,
-                            password: _passwordController.text
-                          )).user;
-                        
-                        Navigator.pushNamed(context, '/');
-                      } catch (e) {
-                        print('Error Happened!!!: $e');
-                      }  
+      body: SingleChildScrollView(
+        child: Form(
+          key: _registerFormKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: 'Name', hintText: "John Jackson"),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Please enter name.";
                     }
+                    return null;
                   },
                 ),
-              ),
-              Text("Already have an account?"),
-              FlatButton(
-                child: Text("Login here!"),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                },
-              ),
-            ]
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: 'Email', hintText: "johnjackson@example.com"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter email';
+                    } else if (!EmailValidator.validate(value)) {
+                      // Use plugin https://pub.dev/packages/email_validator
+                      // If you don't want to use plugin https://stackoverflow.com/questions/16800540/validate-email-address-in-dart
+                      return 'Please enter valid email';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter password';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  decoration: InputDecoration(labelText: 'Confirm Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    // https://stackoverflow.com/questions/50155348/how-to-validate-a-form-field-based-on-the-value-of-the-other
+                    if (value != _passwordController.text) {
+                      return 'Password is not matching';
+                    }
+                    return null;
+                  },
+                ),
+                Container(
+                  // https://stackoverflow.com/questions/50186555/how-to-set-margin-for-a-button-in-flutter
+                  margin: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                  child: RaisedButton(
+                    child: Text("Register"),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    onPressed: () async {
+                      if (_registerFormKey.currentState.validate()) {
+                        try {
+                          // Register user by firebase auth
+                          final FirebaseUser user = (await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                              email: _emailController.text,
+                              password: _passwordController.text
+                            )).user;
+                          
+                          Navigator.pushNamed(context, '/');
+                        } catch (e) {
+                          print('Error Happened!!!: $e');
+                        }  
+                      }
+                    },
+                  ),
+                ),
+                Text("Already have an account?"),
+                FlatButton(
+                  child: Text("Login here!"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                ),
+              ]
+            ),
           ),
         ),
       ),
