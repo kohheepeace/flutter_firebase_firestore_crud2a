b9275fc90119d6ac91a6fdfb829a917ed5bf82c0
kohheepeace <kohheepeace@gmail.com>
2020-02-17 02:43:26 +0700

    Finish Step9 Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,96 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'register_page.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
+
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login Page"),
       ),
-      body: Center(
-        child: Text('This is login page')
+      body: Form(
+        key: _loginFormKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email', hintText: "johnjackson@example.com"),
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter email';
+                  } else if (!EmailValidator.validate(value)) {
+                    return 'Please enter valid email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                controller: _passwordController,
+                obscureText: true,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Please enter password';
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: RaisedButton(
+                  child: Text("Login"),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  onPressed: () async {
+                    if (_loginFormKey.currentState.validate()) {
+                      try {                        
+                        final FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        )).user;
+                        Navigator.pushNamed(context, '/');
+                      } catch (e) {
+                        print('Error Happened!!!: $e');
+                      }  
+                    }
+                  },
+                ),
+              ),
+              Text("You don't have an account?"),
+              FlatButton(
+                child: Text("Register here!"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
