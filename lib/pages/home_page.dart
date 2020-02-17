0fa3a1751100e7566986d8d71c8399c673e18ed0
kohheepeace <kohheepeace@gmail.com>
2020-02-17 23:30:39 +0700

    Finish Step6-1 Add Drawer widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
@@ -10,9 +14,34 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Center(
-        child: Text('This is home page')
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Login'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Register'),
+            ),
+          ],
+        ),
       ),
+      body: Center(child: Text('This is home page')),
     );
   }
 }
