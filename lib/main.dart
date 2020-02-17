04589c373e5656ff0b34c40d77740f1d548af445
kohheepeace <kohheepeace@gmail.com>
2020-02-17 23:34:22 +0700

    Finish Step6-2 Add Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_firestore_crud2a/pages/home_page.dart';
+import 'package:flutter_firebase_firestore_crud2a/pages/register_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => HomePage(),
+        '/sign_up': (context) => RegisterPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_firestore_crud2a/pages/login_page.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -31,13 +32,26 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Login'),
-            ),
+							leading: Icon(Icons.exit_to_app),
+							title: Text('Login'),
+							onTap: () {
+								// https://flutter.dev/docs/cookbook/navigation/navigation-basics#2-navigate-to-the-second-route-using-navigatorpush
+								// https://stackoverflow.com/questions/43807184/how-to-close-scaffolds-drawer-after-an-item-tap
+								Navigator.pop(context);
+								Navigator.push(
+									context,
+									MaterialPageRoute(builder: (context) => LoginPage()),
+								);
+							}
+						),
             ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Register'),
-            ),
+							leading: Icon(Icons.account_circle),
+							title: Text('Register'),
+							onTap: () {
+								Navigator.pop(context);
+								Navigator.pushNamed(context, '/sign_up');
+							}
+						),
           ],
         ),
       ),
