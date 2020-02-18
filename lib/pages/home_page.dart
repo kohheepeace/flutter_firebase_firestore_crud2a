27dcebec0cebd5c79b14c7e1dd204cfba2f1c9af
kohheepeace <kohheepeace@gmail.com>
2020-02-19 05:19:34 +0700

    Finish Step10-2 Edit Drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_firestore_crud2a/pages/login_page.dart';
+import 'package:flutter_firebase_firestore_crud2a/widgets/home_drawer.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.isAuthenticated}) : super(key: key);
@@ -17,54 +16,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Drawer Header',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-							leading: Icon(Icons.exit_to_app),
-							title: Text('Login'),
-							onTap: () {
-								// https://flutter.dev/docs/cookbook/navigation/navigation-basics#2-navigate-to-the-second-route-using-navigatorpush
-								// https://stackoverflow.com/questions/43807184/how-to-close-scaffolds-drawer-after-an-item-tap
-								Navigator.pop(context);
-								Navigator.push(
-									context,
-									MaterialPageRoute(builder: (context) => LoginPage()),
-								);
-							}
-						),
-            ListTile(
-							leading: Icon(Icons.account_circle),
-							title: Text('Register'),
-							onTap: () {
-								Navigator.pop(context);
-								Navigator.pushNamed(context, '/sign_up');
-							}
-						),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Sign Out'),
-              onTap: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushNamed(context, '/');
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: HomeDrawer(isAuthenticated: widget.isAuthenticated),
       body: Center(
         child: widget.isAuthenticated ? Text('Home Page after login') : Text('Home Page before login')
       ),
---
+++ lib/widgets/home_drawer.dart
@@ -0,0 +1,80 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_firestore_crud2a/pages/login_page.dart';
+import 'package:flutter_firebase_firestore_crud2a/pages/my_posts_page.dart';
+import 'package:flutter_firebase_firestore_crud2a/pages/register_page.dart';
+
+class HomeDrawer extends StatelessWidget {
+  HomeDrawer({Key key, this.isAuthenticated}) : super(key: key);
+  final bool isAuthenticated;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Drawer Header',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          // About this syntax: https://github.com/flutter/flutter/issues/28181#issuecomment-510667527
+          if (isAuthenticated) ...[
+            ListTile(
+              leading: Icon(Icons.note),
+              title: Text('My Posts'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyPostsPage()),
+                );
+              }
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Sign Out'),
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushNamed(context, '/');
+              },
+            ),
+          ],
+          if (!isAuthenticated) ...[
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Login'),
+              onTap: () {
+                // https://flutter.dev/docs/cookbook/navigation/navigation-basics#2-navigate-to-the-second-route-using-navigatorpush
+                // https://stackoverflow.com/questions/43807184/how-to-close-scaffolds-drawer-after-an-item-tap
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              }
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Register'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                );
+              }
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
