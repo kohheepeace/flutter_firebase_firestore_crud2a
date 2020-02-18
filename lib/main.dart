047c58b049d6310ff940fd02709ae5c8f5e5e203
kohheepeace <kohheepeace@gmail.com>
2020-02-19 01:11:26 +0700

    Finish Step10-1-1 Make lib/main.dart StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:flutter_firebase_firestore_crud2a/pages/register_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
