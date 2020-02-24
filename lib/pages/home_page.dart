ce1907b9a453dcb0cd890b0f7857a6f128606c31
kohheepeace <kohheepeace@gmail.com>
2020-02-25 00:09:13 +0700

    WIP

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_firestore_crud2a/pages/login_page.dart';
@@ -6,7 +7,6 @@ import 'package:flutter_firebase_firestore_crud2a/widgets/home_drawer.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -16,14 +16,33 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final user = Provider.of<FirebaseUser>(context);
     final bool isAuthenticated = user != null;
-    
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
       ),
       drawer: HomeDrawer(),
-      body: Center(
-        child: isAuthenticated ? Text('Home Page after login') : Text('Home Page before login')
+      body: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collectionGroup('posts').orderBy('createdAt', descending: true).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Loading...');
+            default:
+              return ListView(
+                children: snapshot.data.documents.map((DocumentSnapshot document) {
+                  return ListTile(
+                    title: Text(
+                      document['title'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(document['content']),
+                  );
+                }).toList(),
+              );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
