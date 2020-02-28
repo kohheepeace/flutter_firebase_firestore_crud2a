b4d4f560fa14c5f4b477dd226f8cff9e8be286c3
kohheepeace <kohheepeace@gmail.com>
2020-02-28 20:20:11 +0700

    Finish Step24-3 write test for our flutter rules

--- server/test/test.js
+++ server/test/test.js
@@ -42,96 +42,122 @@ after(async () => {
 });
 
 describe("My app", () => {
-  it("require users to log in before creating a profile", async () => {
+  it("require users to log in before creating a user", async () => {
+    // mock not isauthenticated user
     const db = authedApp(null);
-    const profile = db.collection("users").doc("alice");
-    await firebase.assertFails(profile.set({ birthday: "January 1" }));
+    // get user document ref
+    const user = db.collection("users").doc("alice");
+
+    const query = user.set({ name: "Alice" });
+    await firebase.assertFails(query);
   });
 
-  it("should enforce the createdAt date in user profiles", async () => {
-    const db = authedApp({ uid: "alice" });
-    const profile = db.collection("users").doc("alice");
-    await firebase.assertFails(profile.set({ birthday: "January 1" }));
-    await firebase.assertSucceeds(
-      profile.set({
-        birthday: "January 1",
-        createdAt: firebase.firestore.FieldValue.serverTimestamp()
-      })
-    );
+  it("should let anyone read any published posts", async () => {  
+    const db = authedApp(null);
+    const queryPublished = db.collectionGroup('posts').where("published", "==", true).get();
+    const queryDrafts = db.collectionGroup('posts').where("published", "==", false).get();
+    await firebase.assertSucceeds(queryPublished);
+    await firebase.assertFails(queryDrafts);
   });
 
-  it("should only let users create their own profile", async () => {
+  it("should only let users to query their own post", async () => {
+    // mock isAuthenticated user (alice)
     const db = authedApp({ uid: "alice" });
     await firebase.assertSucceeds(
       db
         .collection("users")
         .doc("alice")
-        .set({
-          birthday: "January 1",
-          createdAt: firebase.firestore.FieldValue.serverTimestamp()
-        })
+        .collection("posts")
+        .get()
     );
     await firebase.assertFails(
       db
         .collection("users")
         .doc("bob")
-        .set({
-          birthday: "January 1",
-          createdAt: firebase.firestore.FieldValue.serverTimestamp()
-        })
+        .collection("posts")
+        .get()
     );
   });
 
-  it("should let anyone read any profile", async () => {
-    const db = authedApp(null);
-    const profile = db.collection("users").doc("alice");
-    await firebase.assertSucceeds(profile.get());
-  });
-
-  it("should let anyone create a room", async () => {
-    const db = authedApp({ uid: "alice" });
-    const room = db.collection("rooms").doc("firebase");
-    await firebase.assertSucceeds(
-      room.set({
-        owner: "alice",
-        topic: "All Things Firebase"
-      })
-    );
-  });
-
-  it("should force people to name themselves as room owner when creating a room", async () => {
-    const db = authedApp({ uid: "alice" });
-    const room = db.collection("rooms").doc("firebase");
-    await firebase.assertFails(
-      room.set({
-        owner: "scott",
-        topic: "Firebase Rocks!"
-      })
-    );
-  });
-
-  it("should not let one user steal a room from another user", async () => {
+  it("should not allow to read other's draft post", async () => {
     const alice = authedApp({ uid: "alice" });
     const bob = authedApp({ uid: "bob" });
 
-    await firebase.assertSucceeds(
-      bob
-        .collection("rooms")
-        .doc("snow")
-        .set({
-          owner: "bob",
-          topic: "All Things Snowboarding"
-        })
-    );
+    // Make alice's draft post
+    const aliceDraftPost = alice.collection("users").doc("alice").collection("posts").doc("alice-post1");
+    await aliceDraftPost.set({title: "title", published: false});
 
-    await firebase.assertFails(
-      alice
-        .collection("rooms")
-        .doc("snow")
-        .set({
-          owner: "alice",
-          topic: "skiing > snowboarding"
-        })
-    );
+    // Make alice's published post
+    const alicePublicPost = alice.collection("users").doc("alice").collection("posts").doc("alice-post2");
+    await alicePublicPost.set({title: "title", published: true});
+
+    // Bob access alice's draft post
+    const bobQuery1 = bob.collection("users").doc("alice").collection("posts").doc("alice-post1").get();
+
+    // Bob access alice's published post
+    const bobQuery2 = bob.collection("users").doc("alice").collection("posts").doc("alice-post2").get();
+
+    // Alice access alice's draft post
+    const aliceQuery = aliceDraftPost.get();
+
+    await firebase.assertFails(bobQuery1);
+    await firebase.assertSucceeds(bobQuery2);
+    await firebase.assertSucceeds(aliceQuery);
+  });
+
+  it("require users to log in before creating a post", async () => {
+    const db = authedApp(null);
+    const query = db.collection("users").doc("alice")
+                    .collection("posts").doc()
+                    .set({
+                      title: "alice",
+                      content: "All Things Firebase"
+                    })
+    await firebase.assertFails(query);
+  });
+
+  it("requires title field to create a post", async () => {
+    const db = authedApp({ uid: "alice" });
+    const postDocRef = db.collection("users").doc("alice").collection("posts").doc()
+                  
+    await firebase.assertFails(postDocRef.set({title: ""}));
+    await firebase.assertSucceeds(postDocRef.set({title: "title 1"}));
+  });
+
+  it("should not allow to update other's post", async () => {
+    const alice = authedApp({ uid: "alice" });
+    const bob = authedApp({ uid: "bob" });
+
+    // Make bob's post
+    await bob.collection("users").doc("bob")
+      .collection("posts").doc("bobPost1")
+      .set({title: "hogehoge"});
+
+    // alice query to update bob's post
+    aliceQuery = alice.collection("users").doc("bob")
+                  .collection("posts").doc("bobPost1")
+                  .update({title: "hoge"});
+                  
+    await firebase.assertFails(aliceQuery);
+  });
+
+  it("requires title field to update a post", async () => {
+    const db = authedApp({ uid: "alice" });
+    const postDocRef = db.collection("users").doc("alice").collection("posts").doc("post1");
+    await postDocRef.set({title: "hogehoge"});
+                  
+    await firebase.assertFails(postDocRef.update({title: ""}));
+    await firebase.assertSucceeds(postDocRef.update({title: "title 1"}));
+  });
+
+  it("should not allow to delete other's post", async () => {
+    const alice = authedApp({ uid: "alice" });
+
+    // alice query to update bob's post
+    aliceQuery = alice.collection("users").doc("bob")
+                  .collection("posts").doc("bobPost1")
+                  .delete();
+                  
+    await firebase.assertFails(aliceQuery);
   });
 });
